58fc4e392a7545f08e8905f9dc372614efcb2ffa
Rupal Saxena <rupal.saxena@uzh.ch>
2023-03-29 20:00:46 +0200

    docker readme updated

--- docker/start_cpu_docker.sh
+++ docker/start_cpu_docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --privileged -v ${PWD}:/workspace/Monograph-Image-Retrieval -p 0.0.0.0:4141:4141 3dvision/group11/torch-2.0.0:latest bash
+docker run -it --rm --privileged -v ${PWD}:/workspace/Monograph-Image-Retrieval -p 0.0.0.0:4141:4141 rupalsaxena/3dvision_grp11:latest bash
