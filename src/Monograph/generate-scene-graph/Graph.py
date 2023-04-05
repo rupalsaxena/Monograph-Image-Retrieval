22bcc65fc0fd425bfa2be6b0ee744ab7af5e27a7
Rupal Saxena <rupal.saxena@uzh.ch>
2023-04-05 23:09:19 +0200

    GenerateSceneGraph class ready

---
+++ src/Monograph/generate-scene-graph/Graph.py
@@ -0,0 +1,9 @@
+# store graph data in form of object
+class Graph:
+    def __init__(self, fr_cord, to_cord, fr_id, to_id, frame_id=None, scene_id=None):
+        self.from_coord = fr_cord
+        self.to_coord = to_cord
+        self.from_id = fr_id
+        self.to_id = to_id
+        self.frame_id = frame_id
+        self.scene_id = scene_id
---
+++ src/Monograph/generate-scene-graph/config.py
@@ -0,0 +1,3 @@
+fov_near = 2000
+fov_x = 1.0471975803375244
+dist_thresh = 1.5
