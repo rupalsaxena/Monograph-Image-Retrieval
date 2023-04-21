5ecc9b3d87d6bcbcc1e56a01e31e172e92f1a513
Rupal Saxena <rupal.saxena@uzh.ch>
2023-04-21 16:05:13 +0200

    imj object seperated and gitignore added

--- src/Monograph/dataloader/hypersim/dataloader.py
+++ src/Monograph/dataloader/hypersim/dataloader.py
@@ -1,23 +1,6 @@
 import os
 import h5py
-
-class ImgObj:
-    def __init__(self, setting, scene, frame):
-        self.setting = setting
-        self.scene = scene
-        self.frame = frame
-        self.rgb = None
-        self.depth = None
-        self.semantic = None
-    
-    def set_rgb(self, rgb):
-        self.rgb = rgb
-    
-    def set_depth(self, depth):
-        self.depth = depth
-
-    def set_sematic(self, semantic):
-        self.semantic = semantic
+from dataloader.hypersim.ImgObj import ImgObj
 
 
 class hypersim_dataloader:
