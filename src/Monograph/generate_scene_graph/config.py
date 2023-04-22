190cf48e47dfdd0ddf93e054f5ed4cd00e2e51f5
Rupal Saxena <rsaxena@eu-login-02.euler.ethz.ch>
2023-04-22 15:22:15 +0200

    attempt to improvr curve affect

--- src/Monograph/generate_scene_graph/config.py
+++ src/Monograph/generate_scene_graph/config.py
@@ -1,4 +1,4 @@
-FOV_NEAR = 2000
+FOV_NEAR = 1000
 FOV_X = 1.0471975803375244
 DIST_THRESH = 200 # ideal: 1.5, but greater if graph should connect each and every object.
 viz = False # False: from docker, True: local + wanna visualize
