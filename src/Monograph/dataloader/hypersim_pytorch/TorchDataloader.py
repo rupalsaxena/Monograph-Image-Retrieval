4fef49ad11a39634e471e04c44b35a218ec8dab7
Rupal Saxena <rupal.saxena@uzh.ch>
2023-05-07 22:53:49 +0200

    dataloader transform added

--- src/Monograph/dataloader/hypersim_pytorch/TorchDataloader.py
+++ src/Monograph/dataloader/hypersim_pytorch/TorchDataloader.py
@@ -1,9 +1,15 @@
 import torch
+import torchvision as tv
 from torch.utils.data import Dataset
 
 class TorchDataloader(Dataset):
     def __init__(self, data):
         self.data = data
+        self.transform = tv.transforms.Compose([
+                      tv.transforms.Resize((256, 320)),
+                      tv.transforms.ToTensor(),
+                      tv.transforms.Normalize(mean = [0.485, 0.456, 0.406], 
+                                              std = [0.229, 0.224, 0.225])])
         
     def __len__(self):
         return len(self.data)
@@ -14,5 +20,8 @@ class TorchDataloader(Dataset):
 
         input_tensor = torch.tensor(input_data, dtype=torch.float32)
         output_tensor = torch.tensor(output_data, dtype=torch.float32)
+
+        input_tensor = self.transform(input_tensor)
+        output_tensor = self.transform(output_tensor)
         
         return input_tensor, output_tensor
