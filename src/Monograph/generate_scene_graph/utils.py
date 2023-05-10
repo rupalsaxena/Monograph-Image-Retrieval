c37cbbd25e6c597a40666ba37d8fd245ec392ea4
Rupal Saxena <rupal.saxena@uzh.ch>
2023-05-10 15:10:44 +0200

    bug regarding graphs fixed

--- src/Monograph/generate_scene_graph/utils.py
+++ src/Monograph/generate_scene_graph/utils.py
@@ -1,4 +1,6 @@
-def get_pc_rgb(rgb, depth, semantic, sem_uniq, height, width, f_w, f_h):
+import numpy as np
+
+def get_pc_rgb(rgb, distance, semantic, sem_uniq, height, width, f_w, f_h):
     ids_3d_points = {}
     r_values = {}
     g_values = {}
@@ -8,6 +10,8 @@ def get_pc_rgb(rgb, depth, semantic, sem_uniq, height, width, f_w, f_h):
 
     x0 = width//2
     y0 = height//2
+
+    depth = convert_distance_mtrs_depth(distance, int(height), int(width))
 
     for id in sem_uniq:
         ids_3d_points[id] = []
@@ -35,3 +39,15 @@ def get_pc_rgb(rgb, depth, semantic, sem_uniq, height, width, f_w, f_h):
 
     return ids_3d_points, r_values, g_values, b_values, pcd
 
+def convert_distance_mtrs_depth(distance, height, width):
+    # hypersim bug fixed: They called depth distance! So we need to convert distance to get actual depth
+    focal = 886.81
+
+    plane_x = np.linspace((-0.5 * width) + 0.5, (0.5 * width) - 0.5, width).reshape(1, width).repeat(height, 0).astype(np.float32)[:, :, None]
+    plane_y = np.linspace((-0.5 * height) + 0.5, (0.5 * height) - 0.5, height).reshape(height, 1).repeat(width, 1).astype(np.float32)[:, :, None]
+    plane_z = np.full([height, width, 1], focal, np.float32)
+    img_plane = np.concatenate([plane_x, plane_y, plane_z], 2)
+
+    npyDepth = distance / np.linalg.norm(img_plane, 2, 2) * focal
+
+    return npyDepth
